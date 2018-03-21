c56301e3278ef1c176108df55124c5f98a829059
Raúl Enrique Reza del Castillo <32489142+BiggerCrayonZ@users.noreply.github.com>
2018-03-21 17:53:39 -0600

    Ending the Login with GServices
    -Login Google Services ready
    -Format with Bootstrap4

--- src/App.js
+++ src/App.js
@@ -4,23 +4,69 @@ import './App.css';
 
 class App extends Component {
 
-  handleAuth (){
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogOut = this.handleLogOut.bind(this);
+  }
+
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+    });
+  }
+
+  handleAuth() {
     //Crea el provedor de un Google Auth Provider
     var provider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().signInWithPopup(provider)
-    .then(result => console.log(`${result.user.email} ha iniciado sesión`))
-    .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
   }
+
+  handleLogOut() {
+    firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} ha cerrado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  renderLoginButton() {
+    //Si el usuario está logeado
+    if (this.state.user) {
+      return (
+        <div className="card">
+          <img className="card-img-top" src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <div className="card-body">
+            <h5 className="card-title">
+              Hola {this.state.user.displayName}
+            </h5>
+            <button className="card-text btn btn-primary" onClick={this.handleLogOut} >Cerrar Sesión</button>
+          </div>
+        </div>
+      );
+    } else {
+      //Si no lo está 
+      return (
+        <button className="btn btn-primary" onClick={this.handleAuth}>Login con Google</button>
+      );
+    }
+
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
           <h1 className="App-title">InstaCoon</h1>
         </header>
         <p className="App-intro">
-          <button onClick={this.handleAuth}>Login con Google</button>
+          {this.renderLoginButton()}
         </p>
       </div>
     );
